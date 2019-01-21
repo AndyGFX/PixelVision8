a0c52a4b7505fbae4f0192edbcf382bfea2e2bce
AndyGFX <o.kollar@gmail.com>
2019-01-21 11:37:03 +0100

    Fixed tile Animation for SpriteMode in TLuaTiled class

--- AndyGFXDisk/Libs/TLuaTiled.lua
+++ AndyGFXDisk/Libs/TLuaTiled.lua
@@ -7,7 +7,7 @@ TLuaTiled = {}
 function TLuaTiled:New(data)
 
     o = {
-	 		    tmx = data or nil,
+          tmx = data or nil,
           FLIPPED_HORIZONTALLY_FLAG = 0x80000000,
           FLIPPED_VERTICALLY_FLAG = 0x40000000,
           FLIPPED_DIAGONALLY_FLAG = 0x20000000,
@@ -25,7 +25,7 @@ end
 -- Set loaded TMX lua data
 ---------------------------------------------------------------
 function TLuaTiled:SetTmx(tmxData)
-	self.tmx = tmxData
+  self.tmx = tmxData
 end
 
 ---------------------------------------------------------------
@@ -36,13 +36,14 @@ function TLuaTiled:PrepareAnimations()
   -- for all tilesets
   for t=1,#self.tmx.tilesets do
     -- and all tiles inside
-  	for i=1,#self.tmx.tilesets[t].tiles do
+    for i=1,#self.tmx.tilesets[t].tiles do
       -- if exist animation
       if self.tmx.tilesets[t].tiles[i].animation ~=nil then
         -- define additional information
         self.tmx.tilesets[t].tiles[i].frame_id = 1
         self.tmx.tilesets[t].tiles[i].frames = #self.tmx.tilesets[t].tiles[i].animation
         self.tmx.tilesets[t].tiles[i].time = 0
+        self.tmx.tilesets[t].tiles[i].tile = 0
       end
     end
   end
@@ -57,13 +58,14 @@ function TLuaTiled:ResetAnimations()
   -- for all tilesets
   for t=1,#self.tmx.tilesets do
     -- and all tiles inside
-  	for i=1,#self.tmx.tilesets[t].tiles do
+    for i=1,#self.tmx.tilesets[t].tiles do
       -- if exist animation
       if self.tmx.tilesets[t].tiles[i].animation ~=nil then
         -- define additional information
         self.tmx.tilesets[t].tiles[i].id = self.tmx.tilesets[t].tiles[i].animation[1]
         self.tmx.tilesets[t].tiles[i].frame_id = 1
         self.tmx.tilesets[t].tiles[i].time = 0
+        self.tmx.tilesets[t].tiles[i].tile = 0
       end
     end
   end
@@ -71,29 +73,32 @@ end
 
 
 ---------------------------------------------------------------
--- Find TMX layer ID by name
+-- Get animation data for tile id from tilemap
+-- Note: tile id must by dec by -1 !!!!
 ---------------------------------------------------------------
 function TLuaTiled:GetAnimationDataForTile(tid)
   for t=1,#self.tmx.tilesets do
     -- and all tiles inside
-  	for i=1,#self.tmx.tilesets[t].tiles do
+    for i=1,#self.tmx.tilesets[t].tiles do
       -- if exist animation
-      if (self.tmx.tilesets[t].tiles[i].id==tid and self.tmx.tilesets[t].tiles[i].animation ~=nil) then
+      if (self.tmx.tilesets[t].tiles[i].id==(tid-1) and self.tmx.tilesets[t].tiles[i].animation ~=nil) then
         return self.tmx.tilesets[t].tiles[i]
       end
     end
   end
+  
+  return nil
 end
 
 ---------------------------------------------------------------
 -- Find TMX layer ID by name
 ---------------------------------------------------------------
 function TLuaTiled:GetLayerID(layerName)
-	local id = -1
-	for i=1,#self.tmx.layers do
-		if self.tmx.layers[i].name == layerName then return i end
-	end
-	return id
+  local id = -1
+  for i=1,#self.tmx.layers do
+    if self.tmx.layers[i].name == layerName then return i end
+  end
+  return id
 end
 
 
@@ -101,17 +106,32 @@ end
 -- Check if tile has animation
 ---------------------------------------------------------------
 function TLuaTiled:IsAnimated(tid)
-	for t=1,#self.tmx.tilesets do
+ 
+  for t=1,#self.tmx.tilesets do
     -- and all tiles inside
-  	for i=1,#self.tmx.tilesets[t].tiles do
+    for i=1,#self.tmx.tilesets[t].tiles do
       -- if exist animation
-      if (self.tmx.tilesets[t].tiles[i].id==tid and self.tmx.tilesets[t].tiles[i].animation ~=nil) then
+      if (self.tmx.tilesets[t].tiles[i].id==tid-1 and self.tmx.tilesets[t].tiles[i].animation ~=nil) then
         return true
       end
     end
   end
 
-	return false
+  return false
+end
+
+
+---------------------------------------------------------------
+-- Check if tile has animation
+---------------------------------------------------------------
+function TLuaTiled:SetNextFrame(tile)
+  -- reset timer
+  tile.time = 0
+  tile.frame_id = tile.frame_id + 1;
+  if (tile.frame_id>tile.frames) then
+    tile.frame_id = 1
+  end
+  tile.tile = tile.animation[tile.frame_id].tileid
 end
 
 ---------------------------------------------------------------
@@ -119,25 +139,15 @@ end
 ---------------------------------------------------------------
 function TLuaTiled:Update(deltaTime)
 
-	for t=1,#self.tmx.tilesets do
+  for t=1,#self.tmx.tilesets do
     -- and all tiles inside
-  	for i=1,#self.tmx.tilesets[t].tiles do
+    for i=1,#self.tmx.tilesets[t].tiles do
       -- if exist animation
       if (self.tmx.tilesets[t].tiles[i].animation ~=nil) then
           -- add delta time
-          self.tmx.tilesets[t].tiles[i].time = self.tmx.tilesets[t].tiles[i].time + (deltaTime*100)
+          self.tmx.tilesets[t].tiles[i].time = self.tmx.tilesets[t].tiles[i].time + (deltaTime*1000)
           -- if time > 100
-          if (self.tmx.tilesets[t].tiles[i].time>100) then
-            -- reset timer
-            self.tmx.tilesets[t].tiles[i].time = 0
-            -- add frame
-            self.tmx.tilesets[t].tiles[i].frame_id = self.tmx.tilesets[t].tiles[i].frame_id + 1;
-            -- if out of frames
-            if (self.tmx.tilesets[t].tiles[i].frame_id>self.tmx.tilesets[t].tiles[i].frames) then
-              -- set first frame
-              self.tmx.tilesets[t].tiles[i].frame_id = 1
-            end
-          end
+          if (self.tmx.tilesets[t].tiles[i].time>100) then self:SetNextFrame(self.tmx.tilesets[t].tiles[i]) end
       end
     end
   end
@@ -149,7 +159,7 @@ end
 -- Return Tile data on [X,Y] from definend layer
 ---------------------------------------------------------------
 function TLuaTiled:GetTile(layer,x,y)
-  local tid = -1
+  local tid = 0
   local pos = self.tmx.layers[layer].width*y+x
   local fx,fy,fxy = false
   local anim = nil
@@ -167,23 +177,20 @@ function TLuaTiled:GetTile(layer,x,y)
   if (bit32.band(tid,self.FLIPPED_VERTICALLY_FLAG)==self.FLIPPED_VERTICALLY_FLAG) then tile.flip_y = true else tile.flip_y = false end
   if (bit32.band(tid,self.FLIPPED_HORIZONTALLY_FLAG)==self.FLIPPED_HORIZONTALLY_FLAG) then tile.flip_xy = true else tile.flip_xy = false end
 
-  tid = bit32.band(tid,self.CLEAR_FLAG)
-
-  -- ???? doesn work when is included here. From main script works !!!! Why ?????????
-  if self:IsAnimated(tid) then
-    anim = self:GetAnimationDataForTile(tid)
-    tile.id = anim.animation[anim.frame_id].tileid
-  else
-
-    tile.id = tid
+  tile.id = bit32.band(tid,self.CLEAR_FLAG)
+  
+  if (tile.id~=0) then
+    -- ???? doesn work when is included here. From main script works !!!! Why ?????????
+    if self:IsAnimated(tile.id) then
+      anim = self:GetAnimationDataForTile(tile.id)
+      tile.id = anim.tile
+    else
+      tile.id = tile.id-1 
+    end
   end
-
-  if (tile.id~=0) then tile.id = tile.id-1 end
-
-
-	return tile
+  
+  return tile
 end
-
 ---------------------------------------------------------------
 -- Draw tilemap in TILE mode
 ---------------------------------------------------------------
--- AndyGFXDisk/Tests/TST_Tiled/code.lua
+++ AndyGFXDisk/Tests/TST_Tiled/code.lua
@@ -59,24 +59,8 @@ function Draw(timeDelta)
   -- single call.
   RedrawDisplay()
 
-  -- TODO add your own draw logic here.
 
   -- draw tiles to screen with flip flag
   tiled:DrawAsSpriteMap(lid)
-
-  local t97 = tiled:GetTile(lid,9,7)
-  if tiled:IsAnimated(t97.id) then
-
-    anim = tiled:GetAnimationDataForTile(t97.id)
-
-    DrawText("ID       :" .. anim.id, 1, 1*8, DrawMode.Sprite, "large",15)
-    DrawText("frame ID :" .. anim.frame_id, 1, 2*8, DrawMode.Sprite, "large",15)
-    DrawText("frames   :" .. anim.frames, 1, 3*8, DrawMode.Sprite, "large",15)
-    DrawText("tile ID  :" ..  anim.animation[anim.frame_id].tileid, 1, 4*8, DrawMode.Sprite, "large",15)
-    DrawText("Time     :".. anim.time, 1, 5*8, DrawMode.Sprite, "large",15)
-    DrawText("map tile :".. t97.id, 1, 6*8, DrawMode.Sprite, "large",15)
-  end
-
-
 
 end

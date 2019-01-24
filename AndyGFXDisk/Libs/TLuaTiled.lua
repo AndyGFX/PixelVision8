248594e5f679f745fa879ef3fe6b0cb0fb2d885c
AndyGFX <o.kollar@gmail.com>
2019-01-24 19:15:52 +0100

    Redesigned Tiled tile IDX calculation and removed unoptimalized TLuaTiled class

--- AndyGFXDisk/Libs/TLuaTiled.lua
+++ AndyGFXDisk/Libs/TLuaTiled.lua
@@ -8,6 +8,7 @@ function TLuaTiled:New(data)
 
     o = {
           tmx = data or nil,
+          animations = {},
           FLIPPED_HORIZONTALLY_FLAG = 0x80000000,
           FLIPPED_VERTICALLY_FLAG = 0x40000000,
           FLIPPED_DIAGONALLY_FLAG = 0x20000000,
@@ -39,36 +40,47 @@ function TLuaTiled:PrepareAnimations()
     for i=1,#self.tmx.tilesets[t].tiles do
       -- if exist animation
       if self.tmx.tilesets[t].tiles[i].animation ~=nil then
-        -- define additional information
-        self.tmx.tilesets[t].tiles[i].frame_id = 1
-        self.tmx.tilesets[t].tiles[i].frames = #self.tmx.tilesets[t].tiles[i].animation
-        self.tmx.tilesets[t].tiles[i].time = 0
-        self.tmx.tilesets[t].tiles[i].tile = 0
+
+        local hash_id = tostring(self.tmx.tilesets[t].tiles[i].id)
+        self.animations[hash_id]={}
+        self.animations[hash_id].id = self.tmx.tilesets[t].tiles[i].id
+        -- current animation frame id
+        self.animations[hash_id].frame_id = 1
+        -- animations count
+        self.animations[hash_id].frames = #self.tmx.tilesets[t].tiles[i].animation
+        -- animation timer
+        self.animations[hash_id].time = 0
+        -- tile id for render
+        self.animations[hash_id].tile = self.tmx.tilesets[t].tiles[i].id
+        self.animations[hash_id].tiles = self.tmx.tilesets[t].tiles[i].animation
       end
     end
   end
 end
 
+
+function TLuaTiled:DumpData(anim)
+
+  print("Animation ID      : "..anim.id)
+  print("  Frame ID        : "..anim.frame_id)
+  print("  Frames count    : "..anim.frames)
+  print("  Current duration: "..anim.time)
+  print("  Anim Tile ID    : "..anim.tile)
+  print("---------------------------------------")
+
+end
 ---------------------------------------------------------------
 -- Reset additional animation properties from tmx structure
 -- and set exported/default values
 ---------------------------------------------------------------
 function TLuaTiled:ResetAnimations()
 
-  -- for all tilesets
-  for t=1,#self.tmx.tilesets do
-    -- and all tiles inside
-    for i=1,#self.tmx.tilesets[t].tiles do
-      -- if exist animation
-      if self.tmx.tilesets[t].tiles[i].animation ~=nil then
-        -- define additional information
-        self.tmx.tilesets[t].tiles[i].id = self.tmx.tilesets[t].tiles[i].animation[1]
-        self.tmx.tilesets[t].tiles[i].frame_id = 1
-        self.tmx.tilesets[t].tiles[i].time = 0
-        self.tmx.tilesets[t].tiles[i].tile = 0
-      end
-    end
+  for key,value in pairs(self.animations) do
+    self.animations[key].frame_id=1
+    self.animations[key].time=0
+    self.animations[key].tile=self.animations[key].id
   end
+
 end
 
 
@@ -76,18 +88,11 @@ end
 -- Get animation data for tile id from tilemap
 -- Note: tile id must by dec by -1 !!!!
 ---------------------------------------------------------------
-function TLuaTiled:GetAnimationDataForTile(tid)
-  for t=1,#self.tmx.tilesets do
-    -- and all tiles inside
-    for i=1,#self.tmx.tilesets[t].tiles do
-      -- if exist animation
-      if (self.tmx.tilesets[t].tiles[i].id==(tid-1) and self.tmx.tilesets[t].tiles[i].animation ~=nil) then
-        return self.tmx.tilesets[t].tiles[i]
-      end
-    end
-  end
-  
-  return nil
+function TLuaTiled:GetAnimationFrameForTile(tid)
+
+  local hash = tostring(tid)
+  return self.animations[hash].tile
+
 end
 
 ---------------------------------------------------------------
@@ -101,22 +106,44 @@ function TLuaTiled:GetLayerID(layerName)
   return id
 end
 
+---------------------------------------------------------------
+-- Find TilsetID ID by map tile id
+---------------------------------------------------------------
+function TLuaTiled:GetTilsetID(tile_id)
+  local tileset_id = 1
+  for i=2,#self.tmx.tilesets do
+    if (tile_id<=self.tmx.tilesets[i].firstgid) then
+      return tileset_id
+    end
+      tileset_id=tileset_id+1
+  end
+  return tileset_id
+end
+
+---------------------------------------------------------------
+-- Find Tilset name ID by map tile id
+---------------------------------------------------------------
+function TLuaTiled:GetTilsetName(tile_id)
+  local tileset_id = 1
+  for i=2,#self.tmx.tilesets do
+    if (tile_id<=self.tmx.tilesets[i].firstgid) then
+      return self.tmx.tilesets[i-1].name
+    end
+      tileset_id=tileset_id+1
+  end
+  return nil
+end
 
 ---------------------------------------------------------------
 -- Check if tile has animation
 ---------------------------------------------------------------
 function TLuaTiled:IsAnimated(tid)
- 
-  for t=1,#self.tmx.tilesets do
-    -- and all tiles inside
-    for i=1,#self.tmx.tilesets[t].tiles do
-      -- if exist animation
-      if (self.tmx.tilesets[t].tiles[i].id==tid-1 and self.tmx.tilesets[t].tiles[i].animation ~=nil) then
-        return true
-      end
-    end
-  end
 
+  local hash = tostring(tid)
+
+  if (self.animations[hash]~=nil) then
+    return true
+  end
   return false
 end
 
@@ -124,14 +151,15 @@ end
 ---------------------------------------------------------------
 -- Check if tile has animation
 ---------------------------------------------------------------
-function TLuaTiled:SetNextFrame(tile)
-  -- reset timer
-  tile.time = 0
-  tile.frame_id = tile.frame_id + 1;
-  if (tile.frame_id>tile.frames) then
-    tile.frame_id = 1
+function TLuaTiled:SetNextFrame(animation)
+
+  animation.time=0
+  animation.frame_id = animation.frame_id + 1
+  if (animation.frame_id>animation.frames) then
+    animation.frame_id = 1
   end
-  tile.tile = tile.animation[tile.frame_id].tileid
+  animation.tile = animation.tiles[animation.frame_id].tileid
+
 end
 
 ---------------------------------------------------------------
@@ -139,19 +167,15 @@ end
 ---------------------------------------------------------------
 function TLuaTiled:Update(deltaTime)
 
-  for t=1,#self.tmx.tilesets do
-    -- and all tiles inside
-    for i=1,#self.tmx.tilesets[t].tiles do
-      -- if exist animation
-      if (self.tmx.tilesets[t].tiles[i].animation ~=nil) then
-          -- add delta time
-          self.tmx.tilesets[t].tiles[i].time = self.tmx.tilesets[t].tiles[i].time + (deltaTime*1000)
-          -- if time > 100
-          if (self.tmx.tilesets[t].tiles[i].time>100) then self:SetNextFrame(self.tmx.tilesets[t].tiles[i]) end
-      end
-    end
-  end
+  for key,value in pairs(self.animations) do
 
+    value.time = value.time + (deltaTime*1000)
+
+    if (value.time >= value.tiles[value.frame_id].duration) then
+        self:SetNextFrame(value)
+    end
+
+  end
 end
 
 
@@ -178,17 +202,19 @@ function TLuaTiled:GetTile(layer,x,y)
   if (bit32.band(tid,self.FLIPPED_HORIZONTALLY_FLAG)==self.FLIPPED_HORIZONTALLY_FLAG) then tile.flip_xy = true else tile.flip_xy = false end
 
   tile.id = bit32.band(tid,self.CLEAR_FLAG)
-  
+
+  local tileset_id = self:GetTilsetID(tile.id)
+
   if (tile.id~=0) then
-    -- ???? doesn work when is included here. From main script works !!!! Why ?????????
+
+    local real_tile_id = tile.id - self.tmx.tilesets[tileset_id].firstgid
+    tile.id = real_tile_id
+
     if self:IsAnimated(tile.id) then
-      anim = self:GetAnimationDataForTile(tile.id)
-      tile.id = anim.tile
-    else
-      tile.id = tile.id-1 
+      tile.id = self:GetAnimationFrameForTile(tile.id)
     end
   end
-  
+
   return tile
 end
 ---------------------------------------------------------------
--- AndyGFXDisk/Tests/TST_Tiled/code.lua
+++ AndyGFXDisk/Tests/TST_Tiled/code.lua
@@ -3,7 +3,7 @@
 LoadScript("Test_level_33x31.lua")
 local tmxData = Get_Tilemap_000()
 
-LoadScript("TLuaTiledOpt.lua")
+LoadScript("TLuaTiled.lua")
 
 local tiled = nil
 local tid
